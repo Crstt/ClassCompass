056f8fe89c23ad74849e5ad1c96f30d06f384e7a
Crstt <matteo598.catalano@gmail.com>
2023-12-08 17:49:15 -0500

    agenda table

---
+++ ClassCompass/ViewControllers/tableViews/agendaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  agendaTableViewCell.swift
+//  ClassCompass
+//
+//  Created by Matteo Catalano on 12/7/23.
+//
+
+import UIKit
+
+class agendaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
